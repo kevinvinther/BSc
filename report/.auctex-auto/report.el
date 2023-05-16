e7e3ef387830d2386e343893fcf022e723be6605
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-05-16 17:37:14 +0200

    Initial Commit

---
+++ report/.auctex-auto/report.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook
+ "report"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "listings"
+    ""
+    "amssymb"
+    "graphicx"
+    "lipsum"))
+ :latex)
+
