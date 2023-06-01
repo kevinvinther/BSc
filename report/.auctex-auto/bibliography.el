4254138eefd4b262a033f53c9942eea8f02559da
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-06-01 19:07:24 +0200

    Fix bibliography formatting

--- report/.auctex-auto/bibliography.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "bibliography"
- (lambda ()
-   (LaTeX-add-bibitems
-    "kshemkalyani2011distributed")
-   (LaTeX-add-environments
-    '("gathered" LaTeX-env-args ["argument"] 0)))
- '(or :bibtex :latex))
-
